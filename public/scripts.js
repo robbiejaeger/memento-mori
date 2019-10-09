838680c77f85bb005114964e42c2dfd4dd70554f
Robbie Jaeger <jaeger.rob@gmail.com>
2019-10-09 11:32:33 -0600

    Modify gitignore to include public directory which contains: Static files for html, css, and javascript empty service worker file service worker registration notification permission request notification boilerplate

---
+++ public/scripts.js
@@ -0,0 +1,42 @@
+const notifyBtn = document.querySelector('#notify-me');
+
+notifyBtn.addEventListener('click', function() {
+  if (window.Notification) {
+    requestNotificationsPermission();
+
+    if (Notification.permission === 'granted') {
+      sendNotification();
+    }
+  }
+});
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./serviceWorker.js')
+      .then(function(reg) {
+        console.log('Service worker succeefully registered');
+      })
+      .catch(function(err) {
+        console.log('Service worker failed to register: ', err);
+      });
+  });
+}
+
+function requestNotificationsPermission() {
+  Notification.requestPermission(function(status) {
+    // Status is "denied", "granted", or "default"
+    // "default" means they did not select Allow or Block
+    console.log('Notification status is: ', status);
+  });
+}
+
+function sendNotification() {
+  navigator.serviceWorker.getRegistration()
+    .then(function(reg) {
+      const notificationOptions = {
+        body: 'Body text'
+      };
+
+      reg.showNotification('Hello', notificationOptions);
+    });
+}
