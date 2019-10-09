32a50fc88e832d3cad9854dc0f87307f18a3bdd1
Robbie Jaeger <jaeger.rob@gmail.com>
2019-10-09 13:37:50 -0600

    Add function to obtain notification subscription

--- public/scripts.js
+++ public/scripts.js
@@ -3,9 +3,10 @@ const notifyBtn = document.querySelector('#notify-me');
 notifyBtn.addEventListener('click', function() {
   if ('Notification' in window && navigator.serviceWorker) {
     requestNotificationsPermission();
-
     console.log(Notification.permission)
+
     if (Notification.permission === 'granted') {
+      subscribeUser();
       sendNotification();
     }
   }
@@ -15,10 +16,10 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('./serviceWorker.js')
       .then(function(reg) {
-        console.log('Service worker succeefully registered');
+        console.log('Service worker successfully registered');
       })
       .catch(function(err) {
-        console.log('Service worker failed to register:', err);
+        console.error('Service worker failed to register:', err);
       });
   });
 }
@@ -40,4 +41,24 @@ function sendNotification() {
 
       reg.showNotification('Hello', notificationOptions);
     });
+}
+
+function subscribeUser() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(function(reg) {
+
+      reg.pushManager.subscribe({
+        userVisibleOnly: true
+        // Need VAPID key (public) from server
+      }).then(function(sub) {
+        console.log('Subscription object:', sub);
+      }).catch(function(e) {
+        if (Notification.permission === 'denied') {
+          console.warn('Permission for notifications was denied');
+        } else {
+          console.error('Unable to subscribe to push', e);
+        }
+      });
+    })
+  }
 }
